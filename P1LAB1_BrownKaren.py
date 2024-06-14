aef07f857797f842cbd4c48dfa281e1d1354e0b4
brownk1101 <brownk1101@student.faytechcc.edu>
2024-06-14 07:00:27 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_BrownKaren.py
@@ -0,0 +1,14 @@
+#Karen Brown
+#June 13, 2024
+#P1LAB1
+#This application will allow the user to enter their first and last name and it will return a greeting statement.
+#Ask the user to enter their first name and assign it to the variable first_name 
+#Ask the user to enter their last name and assign it to the variable last_name
+#Print the greeting, including the variable first_name and last_name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello", first_name, last_name,end="")
+print("! Welcome to CTI-110")
+
+
